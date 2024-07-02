94af51af367b9422ab7353d74e4537f1ce534fb7
Jaak Ristioja <jaak@ristioja.ee>
2024-07-03 01:52:35 +0300

    Unvendor gtest and gmock

--- tests/tests.h
+++ tests/tests.h
@@ -2,23 +2,6 @@
 
 #if !defined(_WIN32_WINDOWS)
 
-// The following includes necessary gTest/gMock source files for unit tests.
-// Launch DOSBox-X with -tests option in debug builds to run unit tests.
-
-#include "gmock/gmock.cc"
-#include "gmock/gmock-cardinalities.cc"
-#include "gmock/gmock-internal-utils.cc"
-#include "gmock/gmock-matchers.cc"
-#include "gmock/gmock-spec-builders.cc"
-#include "gtest/gtest.cc"
-#include "gtest/gtest-death-test.cc"
-#include "gtest/gtest-filepath.cc"
-#include "gtest/gtest-matchers.cc"
-#include "gtest/gtest-port.cc"
-#include "gtest/gtest-printers.cc"
-#include "gtest/gtest-test-part.cc"
-#include "gtest/gtest-typed-test.cc"
-
 // The following are source files containing unit tests.
 
 #include "dos_files_tests.cpp"
@@ -30,7 +13,7 @@
 //google test code causes problem on win9x, remove them and add empty implementations for linkage.
 
 #include <assert.h>
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 namespace testing {
 void InitGoogleTest(int* argc, char** argv) {}
 
